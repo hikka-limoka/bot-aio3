923fc73609159635f19621503e69ebd4af2cbe01
MuRuLOSE <podryazhchik@mail.ru>
2024-08-16 10:21:42 +0300

    added create_user and get_user

--- app/api.py
+++ app/api.py
@@ -1,6 +1,9 @@
 import aiohttp
 
 class LimokaAPI:
+    def __init__(self, token):
+        self.token = token
+
     async def get_all_modules(self) -> list:
         async with aiohttp.ClientSession() as session:
             async with session.get('https://limoka.vsecoder.dev/api/module/all') as response:
@@ -23,3 +26,19 @@ class LimokaAPI:
             async with session.get('https://limoka.vsecoder.dev/api/user/count') as response:
                 data = await response.json()
                 return data["count"]
+            
+    async def get_user(self, userid: str) -> bool:
+        async with aiohttp.ClientSession() as session:
+            async with session.get('https://limoka.vsecoder.dev/api/user/{user_id}?tg_id=' + userid) as response:
+                data = await response.json()
+                if data.get("error"):
+                    return False
+                else:
+                    return True
+                
+    async def create_user(self, userid: str) -> dict:
+        headers = {"token": self.token}
+
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.post('https://limoka.vsecoder.dev/api/user/?telegram_id=' + userid) as response:
+                return userid
