0e33aa844d8cce8d10730d6fed910290273c0c7f
Всеволод Остащенко <kreepmeister@gmail.com>
2024-08-18 11:45:45 +0300

    Fix image updates from git

--- app/handlers/user/start.py
+++ app/handlers/user/start.py
@@ -3,6 +3,7 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 from app.keyboards.inline import start_keyboard
+import random
 
 router = Router()
 
@@ -14,8 +15,10 @@ async def cmd_start(message: Message, api):
     if not await api.get_user(uid):
         await api.create_user(uid)
 
+    v = random.randint(1, 10**6)
+
     await message.answer_photo(
-        "https://raw.githubusercontent.com/hikka-limoka/stuff/main/banners/start.png",
+        f"https://raw.githubusercontent.com/hikka-limoka/stuff/main/banners/start.png?v={v}",
         "<b>🍾 Modules are now in one place with easy searching!</b>\n"
         "🔎 Start searching:",
         reply_markup=start_keyboard(),
